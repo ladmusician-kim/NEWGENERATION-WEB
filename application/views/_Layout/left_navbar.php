febadf6d25b32e825d2815c826744cbf4cc1a5d9
김동진 <ladmusician.kim@gmail.com>
2015-07-20 17:18:11 +0900

    devide header code

---
+++ application/views/_Layout/left_navbar.php
@@ -0,0 +1,84 @@
+<!-- LEFT NAV -->
+<nav class="menu" id="menu">
+	<div class="menu-scroll">
+		<div class="menu-wrap">
+			<div class="menu-content">
+				<a class="menu-logo" href="index.html">Material</a>
+				<ul class="nav">
+					<li>
+						<a href="ui-card.html">Cards</a>
+					</li>
+					<li>
+						<a href="ui-collapse.html">Collapsible Regions</a>
+					</li>
+					<li>
+						<a href="ui-dropdown.html">Dropdowns</a>
+					</li>
+					<li>
+						<a href="ui-modal.html">Modals &amp; Toasts</a>
+					</li>
+					<li>
+						<a href="ui-nav.html">Navs</a>
+					</li>
+					<li>
+						<a href="ui-progress.html">Progress Bars</a>
+					</li>
+					<li>
+						<a href="ui-tab.html">Tabs</a>
+					</li>
+					<li>
+						<a href="ui-tile.html">Tiles</a>
+					</li>
+				</ul>
+				<hr>
+				<ul class="nav">
+					<li>
+						<a href="ui-button.html">Buttons</a>
+					</li>
+					<li>
+						<a href="ui-form.html">Form Elements</a>
+						<span class="menu-collapse-toggle collapsed" data-target="#form-elements" data-toggle="collapse"><i class="icon menu-collapse-toggle-close">close</i><i class="icon menu-collapse-toggle-default">add</i></span>
+						<ul class="menu-collapse collapse" id="form-elements">
+							<li>
+								<a href="ui-form-adv.html">Form Elements <small>(materialised)</small></a>
+							</li>
+						</ul>
+					</li>
+					<li>
+						<a href="ui-icon.html">Icons</a>
+					</li>
+					<li>
+						<a href="ui-table.html">Tables</a>
+					</li>
+				</ul>
+				<hr>
+				<ul class="nav">
+					<li>
+						<a href="page-affix.html">Full-Width Page <small>(with fixed LHC/RHC)</small></a>
+					</li>
+					<li>
+						<a href="page-palette.html">Page Palettes</a>
+						<span class="menu-collapse-toggle collapsed" data-target="#page-palettes" data-toggle="collapse"><i class="icon menu-collapse-toggle-close">close</i><i class="icon menu-collapse-toggle-default">add</i></span>
+						<ul class="menu-collapse collapse" id="page-palettes">
+							<li>
+								<a href="page-palette-blue.html">Blue Palette</a>
+							</li>
+							<li>
+								<a href="page-palette-green.html">Green Palette</a>
+							</li>
+							<li>
+								<a href="page-palette-purple.html">Purple Palette</a>
+							</li>
+							<li>
+								<a href="page-palette-red.html">Red Palette</a>
+							</li>
+							<li>
+								<a href="page-palette-yellow.html">Yellow Palette</a>
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</nav>
---
+++ application/views/_Layout/profile_navbar.php
@@ -0,0 +1,31 @@
+<!-- PROFILE NAV -->
+<nav class="menu menu-right" id="profile">
+	<div class="menu-scroll">
+		<div class="menu-wrap">
+			<div class="menu-top">
+				<div class="menu-top-img">
+					<img alt="John Smith" src="/NEWGENERATION/library/img/landscape.jpg">
+				</div>
+				<div class="menu-top-info">
+					<a class="menu-top-user" href="javascript:void(0)"><span class="avatar pull-left"><img alt="alt text for John Smith avatar" src="/NEWGENERATION/library/img/avatar-001.jpg"></span>John Smith</a>
+				</div>
+				<div class="menu-top-info-sub">
+					<small>Some additional information about John Smith</small>
+				</div>
+			</div>
+			<div class="menu-content">
+				<ul class="nav">
+					<li>
+						<a href="javascript:void(0)"><span class="icon icon-lg">account_box</span>Profile Settings</a>
+					</li>
+					<li>
+						<a href="javascript:void(0)"><span class="icon icon-lg">add_to_photos</span>Upload Photo</a>
+					</li>
+					<li>
+						<a href="page-login.html"><span class="icon icon-lg">exit_to_app</span>Logout</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</nav>
