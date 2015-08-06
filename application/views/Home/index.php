380fef6df1ea89c51bd0559a7fff45c9a3bc82b1
김동진 <ladmusician.kim@gmail.com>
2015-08-06 15:35:49 +0900

    user guide 삭

--- application/views/Home/index.php
+++ application/views/Home/index.php
@@ -1,9 +1,21 @@
 <div class="content">
+    <!--
     <div class="content-heading">
         <div class="container">
             <div class="row">
                 <div class="col-lg-6 col-lg-push-3 col-sm-10 col-sm-push-1">
                     <h1 class="heading">PROGRAMMING LIKE CRAZY</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    -->
+    <div class="content-inner">
+        <div class="container">
+            <h2 class="content-sub-heading">프로젝트</h2>
+            <div class="card-wrap">
+                <div class="row" id="ng-project-list">
+
                 </div>
             </div>
         </div>
